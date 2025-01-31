5bfcf3a2d63e1f85617304a5ff97deccd81dc2a2
Shubham Malani <skmalani05@gmail.com>
2025-01-31 14:40:41 +0530

    new changes

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path,include
+from .import views
 
 urlpatterns = [
-    path("",include('app.urls'))
+    path("",views.LoginPage,name="loginpage"),
+    path("register/",views.RegisterPage,name="register"),
+    path("auth_regi/",views.UserRegister,name="auth_regi"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+
+
+
+
+def LoginPage(request):
+    return render(request,"app/login.html")
+
+def UserRegister(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        contact = request.POST['contact']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+
+        user = User.objects.filter(Email=email).first()
+
+        if user:
+            message = "User Already Exists"
+            return render(request,"app/register.html",{'msg':message})
+        else:
+            if password == cpassword:
+                new_user = User.objects.create(
+                    Name = name,
+                    Email = email,
+                    Contact = contact,
+                    Password=password
+                )
+                return render(request,"app/login.html")
+            else:
+                message = "Password and Confirm Password Doesn't match"
+                return render(request,"app/register.html",{'msg':message})
+            
+
+
+def RegisterPage(request):
+    return render(request,"app/register.html")
