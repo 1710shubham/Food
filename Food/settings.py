81535b3a4a887128b17388674c6fc47efb32f282
Your Name <jaym.iihglobal@gmail.com>
2025-02-03 13:02:45 +0530

    template integrated

--- Food/settings.py
+++ Food/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'zayka',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
